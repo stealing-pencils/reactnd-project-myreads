3e130fede1760ae23d5e26e86643a93ea56a8cd5
Katherine Riley <unoscarto@hotmail.com>
2018-09-21 10:53:12 -0700

    created main structure in components

---
+++ src/MainPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+
+
+}
+
+export default MainPage;
---
+++ src/SearchPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class SearchPage extends Component {
+
+
+}
+
+export default SearchPage;
