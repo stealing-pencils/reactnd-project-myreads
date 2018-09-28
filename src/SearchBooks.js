8460b73c932dd6ebdc8b953156584d1e6bf71ad7
Katherine Riley <unoscarto@hotmail.com>
2018-09-28 11:31:55 -0700

    added form serialize

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import serializeForm from 'form-serialize'
 
 
 class SearchPage extends Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const values = searlizeForm(e.target,
+      {hash: true})
+      
+  }
+
   render () {
     return (
       <div className="search-books">
@@ -17,7 +26,10 @@ class SearchPage extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
+            <form onSubmit={this.handleSubmit}
+              className="search books">
+              <input type="text" placeholder="Search by title or author"/>
+            </form>
 
           </div>
         </div>
