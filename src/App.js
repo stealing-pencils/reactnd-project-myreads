b504b317948c5dc9eb529ac0194fca19829e1d20
Katherine Riley <unoscarto@hotmail.com>
2018-10-06 10:09:27 -0700

    update call correction now causes books to change shelves with no bug

--- src/App.js
+++ src/App.js
@@ -18,9 +18,10 @@ class BooksApp extends React.Component {
   }
 
   changeBookShelf= (book, shelf) => {
-    BooksAPI.update(book,shelf)
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books })
+    BooksAPI.update(book,shelf).then(() => {
+      BooksAPI.getAll().then((books) => {
+        this.setState({ books })
+      })
     })
   }
 
